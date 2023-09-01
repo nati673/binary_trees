7be65719cfd2babfaa5c3fd3245a450751cafbb9
root <pawlos birhanu>
2023-09-01 04:29:40 -0700

    added

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,13 +11,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new;
+binary_tree_t *news;
 news = malloc(sizeof(binary_tree_t));
-if (!new)
+if (!news)
 return (NULL);
-new->n = value;
-new-n->parent = parent;
-new-n->left = NULL;
-new-n->right = NULL;
-return (new);
+news->n = value;
+news->parent = parent;
+news->left = NULL;
+news->right = NULL;
+return (news);
 }
