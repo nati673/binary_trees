5e6ee479b46663756a7f94ec2ca42c01e5e5d2a0
Pawlos1990 <123082236+Pawlos1990@users.noreply.github.com>
2023-09-01 04:14:47 -0700

    0-binary_tree_node.c new

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_node - crate binary tree node.
- * @parent: to the parent of the node to create new.
+ * @parent:  to create new.
  * @value: value to put in the new node.
  *
  * return: If an error occurs - NULL.
@@ -11,13 +11,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *new;
-new = malloc(sizeof(binary_tree_t));
-if (!new)
+binary_tree_t *new-n;
+new-n = malloc(sizeof(binary_tree_t));
+if (!new-n)
 return (NULL);
-new->n = value;
-new->parent = parent;
-new->left = NULL;
-new->right = NULL;
-return (new);
+new-n->n = value;
+new-n->parent = parent;
+new-n->left = NULL;
+new-n->right = NULL;
+return (new-n);
 }
